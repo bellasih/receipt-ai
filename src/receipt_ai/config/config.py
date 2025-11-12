93b7e79d30dcd0f395be863e7378931cbb799a5b
Bella Septina <bellasih@Bellas-MacBook-Pro-2.local>
2025-11-12 20:54:54 +0700

    init commit ui_platform branch

--- src/receipt_ai/config/config.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path  
-from dynaconf import Dynaconf  
-  
-settings = Dynaconf(  
-    root_path=Path(__file__).parent,  
-    envvar_prefix="DYNACONF",  
-    settings_files=["settings.toml", ".secrets.toml"],  
-)
--- src/receipt_ai/utils/validate_request.py
+++
@@ -1,7 +0,0 @@
-from pydantic import  BaseModel
-from typing import List
-
-class RequestState(BaseModel):
-  system_prompt: str
-  messages: List[str]
-  history_data: List[dict]
